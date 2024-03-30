cb00b80d89d68d47e73120a749d5abe4308ce2e0
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2024-03-30 10:10:18 +0900

    csv 대사관리 1차 완료

--- Assets/01_Scripts/NPC/DialogManager.cs
+++ Assets/01_Scripts/NPC/DialogManager.cs
@@ -1,9 +1,10 @@
+using Photon.Pun;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogManager : MonoBehaviour
+public class DialogManager : MonoBehaviourPun
 {
     private static DialogManager instance;
     public static DialogManager Instance
@@ -40,40 +41,34 @@ public class DialogManager : MonoBehaviour
     //store csv file using public
     public TextAsset dialogCSV;
     // Start is called before the first frame update
-    public string[] EventName;
-    public string[] Dialog;
+    public Dictionary<string, string> DialogDictionary;
     void Start()
     {
+        DialogDictionary = new Dictionary<string, string>();
         //첫줄은 헤더이므로 제외하고 데이터만 가져옵니다.
         string[] data = dialogCSV.text.Split(new char[] { '\n' });
 
         for (int i = 0; i < data.Length; i++)
         {
-            EventName = new string[data.Length];
-            Dialog = new string[data.Length];
-
+            if (data[i] == "")
+            {
+                continue;
+            }
             string[] row = data[i].Split(new char[] { ',' });
-            EventName[i] = row[0];
-            Dialog[i] = row[1];
-            Debug.Log(EventName[i] + " : " + Dialog[i]);
+            DialogDictionary.Add(row[0], row[1]);
         }
 
     }
-
+    // 대사가 많아지면 미리 분리해놓고 사용하기도 고려.
     public string GetDialog(string eventName)
     {
-        for (int i = 0; i < EventName.Length; i++)
+        // If the dictionary contains the key, return the value
+        if (DialogDictionary.ContainsKey(eventName))
         {
-            if (eventName.Equals(this.EventName[i]))
-            {
-                return Dialog[i];
-            }
+            return DialogDictionary[eventName];
         }
-
         // Add a default return statement
         return null;
     }
-
-
 
 }
