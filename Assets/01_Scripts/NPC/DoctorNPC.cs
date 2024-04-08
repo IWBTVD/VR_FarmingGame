13db295f30d12ac8d7876bfe0ef9131628355929
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2024-04-08 11:28:03 +0900

    병원NPC 대사 추가

---
+++ Assets/01_Scripts/NPC/DoctorNPC.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+namespace Jun
+{
+    public class DoctorNPC : NPCbase
+    {
+        public override void Talk()
+        {
+            StopCoroutine(TalkUICoroutine());
+            StartCoroutine(TalkUICoroutine());
+
+        }
+        /// <summary>
+        /// 대화창을 띄우는 코루틴 함수, 2초 후에 대화창을 닫습니다.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator TalkUICoroutine()
+        {
+            talkUI.SetActive(true);
+            Debug.Log("Interacting with TestNPC");
+            string talk1 = GetDialog("Doctor");
+            Debug.Log(talk1);
+            TalkText.text = talk1;
+            yield return new WaitForSeconds(2f);
+
+            // 2초 후에 UI 창을 비활성화합니다.
+            talkUI.SetActive(false);
+        }
+    }
+
+}
