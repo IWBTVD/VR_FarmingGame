caf3a815e9f138e0746365d29d9259912b37ed2c
Gun Park <kun1998@naver.com>
2024-06-27 00:11:26 +0900

    Interactable Scene 생성
    이 씬에서 Collectable 오브젝트 관련하여 작업하면 됩니다.

---
+++ Assets/01_Scripts/Rover Expedition/Collectable/Collectable.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoverExpedition
+{
+
+    public class Collectable : MonoBehaviour
+    {
+        public CollectableSO collectableSO;
+
+
+        public void GetInfo()
+        {
+
+        }
+
+        /// <summary>
+        /// 채집 시도하기
+        /// </summary>
+        public void TryCollect()
+        {
+
+        }
+
+        /// <summary>
+        /// 채집 성공
+        /// </summary>
+        public void OnCollected()
+        {
+
+        }
+    }
+}
